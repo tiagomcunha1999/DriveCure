64060966ca24d7e8a0897211b44fef01de860acd
Tiago Cunha <1700597@sal.ipg.pt>
2020-04-24 18:10:41 +0100

    Criação dos botoes do layout principal e das classes

---
+++ app/src/main/java/pt/ipg/drivecure/NovaEntrega.java
@@ -0,0 +1,14 @@
+package pt.ipg.drivecure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovaEntrega extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_entrega);
+    }
+}
