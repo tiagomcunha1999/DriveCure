2dd75d8ad6436f0f3d7ebd30d5758ef5d7aa01b3
Tiago Cunha <1700597@sal.ipg.pt>
2020-06-22 16:03:35 +0100

    Gets e Sets Funcionarios

---
+++ app/src/main/java/model/mdFuncionarios.java
@@ -0,0 +1,41 @@
+package model;
+
+public class mdFuncionarios {
+
+    private Long id;
+    private String nomeFuncionario;
+    private String emailFuncionario;
+    private String numeroFuncionario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getEmailFuncionario() {
+        return emailFuncionario;
+    }
+
+    public void setEmailFuncionario(String emailFuncionario) {
+        this.emailFuncionario = emailFuncionario;
+    }
+
+    public String getNumeroFuncionario() {
+        return numeroFuncionario;
+    }
+
+    public void setNumeroFuncionario(String numeroFuncionario) {
+        this.numeroFuncionario = numeroFuncionario;
+    }
+}
