610fdf39953ba05da8881c8d17cdd43eac376c27
Tiago Cunha <1700597@sal.ipg.pt>
2020-06-26 09:58:10 +0100

    Criação de funcionarios correção de erros e criação de tabela para inserir os mesmos

--- app/src/main/java/BaseDados/BaseDadosHelper.java
+++ app/src/main/java/BaseDados/BaseDadosHelper.java
@@ -1,21 +1,26 @@
 package BaseDados;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import pt.ipg.drivecure.Funcionarios;
-
 public class BaseDadosHelper extends SQLiteOpenHelper {
 
-    private  Context context;
+    private Context context;
     public static final String DATABASE_NAME = "DiveCure.db";
     public static final int DATABASE_VERSION = 1;
     private static final boolean DESENVOLVIMENTO = true;
 
+    public static final String NOME_TABELA = "funcionarios";
 
+    public static final String CAMPO_NOME_FUNCIONARIO = "nome_funcionario";
+    public static final String CAMPO_ID_FUNCIONARIO = "id_funcionario";
+    public static final String CAMPO_EMAIL_FUNCIONARIO = "email_funcionario";
+    public static final String CAMPO_NUMERO_FUNCIONARIO = "numero_funcionario";
 
 
     public BaseDadosHelper(@Nullable Context context) {
@@ -25,24 +30,49 @@ public class BaseDadosHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        BdTableFuncionarios tabelaFuncionarios = new BdTableFuncionarios(db);
-        tabelaFuncionarios.cria();
-
-        if (DESENVOLVIMENTO) {
-            seedData(db);
-        }
-
+        db.execSQL("CREATE TABLE " + NOME_TABELA + "(" +
+                CAMPO_ID_FUNCIONARIO + "INTERGER PRIMARY KEY AUTOINCREMENT, " +
+                CAMPO_NOME_FUNCIONARIO + " TEXT NOT NULL," +
+                CAMPO_EMAIL_FUNCIONARIO + " TEXT NOT NULL," +
+                CAMPO_NUMERO_FUNCIONARIO + " TEXT NOT NULL" +
+                ")");
     }
 
-    public void seedData(SQLiteDatabase db){
+    /* public void seedData(SQLiteDatabase db){
 
         BdTableFuncionarios tabelaFuncionarios = new BdTableFuncionarios(db);
 
 
-    }
+    } */
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        /*
+        db.execSQL("DROP TABLE IF EXISTS "+ NOME_TABELA);
+        onCreate(db);
+        */
+
+    }
+
+    boolean addFuncionario(String nomeFuncionario, String emailFuncionario, String numeroFuncionario) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(CAMPO_NOME_FUNCIONARIO, nomeFuncionario);
+        cv.put(CAMPO_EMAIL_FUNCIONARIO, emailFuncionario);
+        cv.put(CAMPO_NUMERO_FUNCIONARIO, numeroFuncionario);
+
+        long resultado = db.insert(NOME_TABELA, null, cv);
+
+        if (resultado == -1) {
+            Toast.makeText(context, "Registo Falhado", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            Toast.makeText(context, "Registado com sucesso", Toast.LENGTH_SHORT).show();
+            return true;
+
+        }
     }
 }
--- app/src/main/java/pt/ipg/drivecure/Funcionarios.java
+++ app/src/main/java/pt/ipg/drivecure/Funcionarios.java
@@ -1,24 +1,38 @@
 package pt.ipg.drivecure;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Funcionarios extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    FloatingActionButton floatingActionButtonFun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funcionarios);
 
+        recyclerView = findViewById(R.id.recyclerViewFuncionarios);
+        floatingActionButtonFun = findViewById(R.id.recyclerViewFuncionarios);
+
+        floatingActionButtonFun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Funcionarios.this, novoFuncionario.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void novoFuncionario (View view){
 
-        Intent intent = new Intent(this, novoFuncionario.class);
-        startActivity(intent);
-    }
 
 }
