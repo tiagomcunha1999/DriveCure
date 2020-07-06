a6c708b9c00074545237837d9f57a330bb6a259e
Tiago Cunha <1700597@sal.ipg.pt>
2020-07-06 15:41:39 +0100

    Cursor para mostrar dados na recycler view

--- app/src/main/java/pt/ipg/drivecure/Funcionarios.java
+++ app/src/main/java/pt/ipg/drivecure/Funcionarios.java
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class Funcionarios extends AppCompatActivity {
 
     RecyclerView recyclerViewFun;
     FloatingActionButton floatingButtonFun;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> id_funcionario, nome_funcionario, email_funcionario, contacto_funcionario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +37,29 @@ public class Funcionarios extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        myDB = new MyDatabaseHelper(Funcionarios.this);
+
+        id_funcionario = new ArrayList<>();
+        nome_funcionario = new ArrayList<>();
+        email_funcionario = new ArrayList<>();
+        contacto_funcionario = new ArrayList<>();
+
+    }
+
+    void displayFuncionarios(){
+        Cursor cursor = myDB.lerFuncionarios();
+
+        if(cursor.getCount() == 0){
+            Toast.makeText(this,"Não existem dados", Toast.LENGTH_SHORT).show();
+        }else{
+            while (cursor.moveToNext()){
+                id_funcionario.add(cursor.getString(0));
+                nome_funcionario.add(cursor.getString(1));
+                email_funcionario.add(cursor.getString(2));
+                contacto_funcionario.add(cursor.getString(3));
+            }
+
+        }
     }
 }
